6eef2f535feb49b483617488ff0c8a9f844940f2
brunobeise <bruno_beise@hotmail.com>
2024-02-06 15:40:15 -0300

    eslint bugs

--- src/pages/relatorio/financiamentoxavista/PreviewReport.tsx
+++ src/pages/relatorio/financiamentoxavista/PreviewReport.tsx
@@ -1,11 +1,11 @@
 import { Card } from "@/components/ui/card";
-import { FinanceOrCashReportContext } from "./Context";
-import { useContext } from "react";
+// import { FinanceOrCashReportContext } from "./Context";
+// import { useContext } from "react";
 import Cover from "./Report/Cover";
 import "./Report/index.css";
 
 export default function PreviewRelatorio() {
-  const { financeOrCashReportState } = useContext(FinanceOrCashReportContext);
+  // const { financeOrCashReportState } = useContext(FinanceOrCashReportContext);
 
   return (
     <Card className="lg:col-span-7 uw:col-span-6 border border-border !w-[210mm] !h-[297mm] w-full shadow p-5 light relative justify-content-start">
