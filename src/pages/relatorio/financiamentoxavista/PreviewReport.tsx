3506e18448495e8b5600078aab3715a7cbafc5ff
brunobeise <bruno_beise@hotmail.com>
2024-02-06 20:39:31 -0300

    fix eslint

--- src/pages/relatorio/financiamentoxavista/PreviewReport.tsx
+++ src/pages/relatorio/financiamentoxavista/PreviewReport.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card";
 import Cover from "./Report/Cover";
 import "./Report/index.css";
 
-const PreviewRelatorio = forwardRef<HTMLDivElement>((props, ref) => {
+const PreviewRelatorio = forwardRef<HTMLDivElement>((_props, ref) => {
   return (
     <Card className="lg:col-span-7 uw:col-span-6 border border-border !w-[210mm] !h-[297mm] w-full shadow p-5 light relative justify-content-start !bg-whitefull">
       <div className="!bg-whitefull" ref={ref}>
