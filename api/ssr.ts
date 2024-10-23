5872e6ccaefdf6fe481d1ade7690a1694e039130
brunobeise <bruno_beise@hotmail.com>
2024-10-23 11:05:49 -0300

    renderpag

--- api/ssr.ts
+++ api/ssr.ts
@@ -2,9 +2,7 @@ import { renderPage } from "vike";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const pageContext = await renderPage({
-    urlOriginal: req.url ?? "",
-  });
+  const pageContext = await renderPage({ urlOriginal: req.url!  });
 
   if (pageContext.httpResponse) {
     const { body, statusCode, headers } = pageContext.httpResponse;
