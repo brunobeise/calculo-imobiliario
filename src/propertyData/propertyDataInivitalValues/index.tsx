028190bfa170abce7ac85dedd4eec1895f432643
brunobeise <bruno_beise@hotmail.com>
2024-11-21 13:11:35 -0300

    fix

--- src/propertyData/propertyDataInivitalValues/index.tsx
+++ src/propertyData/propertyDataInivitalValues/index.tsx
@@ -1,8 +1,6 @@
-import financiamentoxavista from "./financiamentoxavista";
 import financingPlanning from "./financingPlanning";
 
 export function getInitialValues(location: string) {
-  if (location === "/financiamentoxavista") return financiamentoxavista;
   if (location === "/planejamentofinanciamento") return financingPlanning;
   else return financingPlanning;
 }
