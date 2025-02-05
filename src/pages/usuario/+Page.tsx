5386cf4b8564adda03afa493203aaa334af4989d
brunobeise <bruno_beise@hotmail.com>
2025-02-05 16:07:46 -0300

    meus dados

--- src/pages/usuario/+Page.tsx
+++ src/pages/usuario/+Page.tsx
@@ -1,67 +1,24 @@
 import { useEffect, useState } from "react";
-import {
-  FaFacebookSquare,
-  FaInstagram,
-  FaLinkedin,
-  FaSave,
-  FaUser,
-  FaWhatsapp,
-} from "react-icons/fa";
-import { uploadImage } from "@/lib/imgur";
-import { FormLabel, Input } from "@mui/joy";
-import PictureInput from "@/components/inputs/PictureInput";
+
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
-import { editUserData, fetchUserData } from "@/store/userReducer";
-import { User } from "@/types/userTypes";
+import { fetchUserData } from "@/store/userReducer";
 import PageStructure from "@/components/structure/PageStructure";
-import FloatingButtonList from "@/components/shared/FloatingButtonList";
 import { useAuth } from "@/auth";
 import { navigate } from "vike/client/router";
+import { FaEdit, FaRegAddressCard } from "react-icons/fa";
+import { FaUser } from "react-icons/fa6";
+import { LuPhoneCall } from "react-icons/lu";
+import BooleanInputSwitch from "@/components/inputs/SwitchInput";
+import { Button } from "@mui/joy";
+import UserFormModal from "@/components/modals/UserFormModal";
 
 export default function UserConfig() {
   const { isAuthenticated } = useAuth();
 
   const dispatch = useDispatch<AppDispatch>();
   const { userData, loading } = useSelector((state: RootState) => state.user);
-  const [form, setForm] = useState<User>({
-    email: "",
-    createdAt: new Date(),
-    fullName: "",
-    owner: false,
-    phone: "",
-    realEstateId: "",
-    role: "",
-    updatedAt: new Date(),
-    address: "",
-  });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const id = e.target.id;
-    const value = e.target.value;
-
-    setForm((prevForm) => ({
-      ...prevForm,
-      [id]: value,
-    }));
-  };
-
-  const handleSave = async () => {
-    if (!form || !form.id) return;
-
-    let uploadPhoto = form.photo;
-
-    if (form.photo && !form.photo.includes("res.cloudinary.com")) {
-      uploadPhoto = await uploadImage(form.photo);
-    }
-
-    dispatch(
-      editUserData({
-        ...form,
-        realEstate: undefined,
-        photo: uploadPhoto,
-      })
-    );
-  };
+  const [userFormModal, setUserFormModal] = useState(false);
 
   useEffect(() => {
     if (!userData) {
@@ -69,171 +26,99 @@ export default function UserConfig() {
     }
   }, [dispatch, userData]);
 
-  useEffect(() => {
-    if (userData) setForm(userData);
-  }, [userData]);
-
   if (!isAuthenticated) {
     navigate("/");
     return null;
   }
 
-  const header = (
-    <div className="w-full flex items-center text-primary gap-2  text-2xl ms-4 mt-8 mb-3">
-      <FaUser className="text-xl" />
-      <h2 className="font-bold">Meus dados</h2>
+  const header = userData && (
+    <div className="flex items-end justify-between">
+      <div className="w-full flex items-center text-primary gap-2  text-2xl ms-4 mt-8 mb-3">
+        <FaUser className="text-xl" />
+        <h2 className="font-bold">Meus dados</h2>
+      </div>
+      <Button onClick={() => setUserFormModal(true)} endDecorator={<FaEdit />}>
+        Editar
+      </Button>
     </div>
   );
 
-  const contentHeader = !loading && userData && (
-    // <UserSignature2
-    //   title="Assinatura Exemplo"
-    //   desc="Nome do imóvel"
-    //   userData={form}
-    // />
+  const content = userData ? (
+    <div className="grid grid-cols-3 gap-5">
+      <div className="col-span-3 bg-gradient-to-r from-whitefull to-white border border-grayScale-200 text-white p-3 shadow-lg rounded-lg h-40 flex">
+        <div className="flex gap-2">
+          <div className="h-full w-36 aspect-square flex justify-center items-center rounded overflow-hidden">
+            <img
+              className="h-full w-full  object-cover"
+              src={userData?.photo}
+            />
+          </div>
+          <div className="flex flex-col h-full justify-between ms-3 text-primary">
+            <span>
+              <span className="text-lg font-bold ">{userData.fullName}</span>
+              <span className="ms-2 text-sm">{userData.role}</span>
+            </span>
+            <span className="flex gap-2 items-center">
+              <FaRegAddressCard size={20} /> CRECI {userData.creci}
+            </span>
+            <span className="flex gap-2 items-center">
+              <LuPhoneCall size={20} /> {userData.phone}
+            </span>
+            <div className="border border-1 border-primary rounded p-[4px] px-2 w-min text-nowrap">
+              <b className="font-bold">{userData.casesCount} </b> Estudos
+              Realizados
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gradient-to-r from-whitefull to-white border border-grayScale-200 text-white p-4 shadow-lg rounded-lg h-64 relative">
+        <h5 className="font-bold">Imobiliária</h5>
+        <div className="absolute top-[50%] translate-x-[-50%] left-[50%] translate-y-[-50%]">
+          <img className="w-[32]" src={userData.realEstate.logo} alt="" />
+        </div>
+      </div>
+      <div className="bg-gradient-to-r from-whitefull to-white border border-grayScale-200 text-white p-4 shadow-lg rounded-lg h-64 relative">
+        <h5 className="font-bold">Plano</h5>
+        <div className="absolute top-[50%] translate-x-[-50%] left-[50%] translate-y-[-50%]">
+          <h5 className="font-bold text-2xl text-nowrap"> Acesso Completo</h5>
+        </div>
+      </div>
+      <div className="bg-gradient-to-r from-whitefull to-white border border-grayScale-200 text-white p-4 shadow-lg rounded-lg h-64 relative flex flex-col gap-4 justify-between">
+        <div className="flex flex-col gap-4">
+          <h5 className="font-bold">Notificações</h5>
+          <BooleanInputSwitch
+            onChange={() => {}}
+            checked={true}
+            label="Receber notificação de visualizações de proposta"
+          />
+          <BooleanInputSwitch
+            onChange={() => {}}
+            checked={true}
+            label="Receber notificação de atualizações do sitema"
+          />
+        </div>
+        <Button className="w-full">Testar envio</Button>
+      </div>
+    </div>
+  ) : (
     <></>
   );
 
-  const content = (
-    <div className="overflow-y-auto">
-      <div className="grid grid-cols-6 gap-x-5 gap-y-3 ">
-        <div className="col-span-6">
-          <PictureInput
-            bordered
-            value={form?.photo ? [form?.photo] : []}
-            label="Foto:"
-            onChange={(v) =>
-              setForm({
-                ...form,
-                photo: v,
-              })
-            }
-          />
-        </div>
-        <div className="col-span-3">
-          <FormLabel htmlFor="fullName">Nome:</FormLabel>
-          <Input
-            onChange={handleChange}
-            type="text"
-            id="fullName"
-            value={form?.fullName || ""}
-            required
-          />
-        </div>
-        <div className="col-span-3">
-          <FormLabel htmlFor="role">Cargo:</FormLabel>
-          <Input
-            onChange={handleChange}
-            type="text"
-            id="role"
-            value={form?.role}
-            required
-          />
-        </div>
-        <div className="col-span-3">
-          <FormLabel htmlFor="creci">CRECI:</FormLabel>
-          <Input
-            onChange={handleChange}
-            type="text"
-            id="creci"
-            value={form?.creci}
-            required
-          />
-        </div>
-
-        <div className="col-span-3">
-          <FormLabel htmlFor="phone">Telefone:</FormLabel>
-          <Input
-            onChange={handleChange}
-            type="text"
-            id="phone"
-            value={form?.phone}
-            required
-          />
-        </div>
-
-        <div className="col-span-6 grid grid-cols-4 gap-5">
-          <div>
-            <FormLabel htmlFor="whatsapp">Link Whatsapp:</FormLabel>
-            <div className="flex items-center">
-              <FaWhatsapp className="text-xl" />
-
-              <Input
-                className="w-full ms-2"
-                onChange={handleChange}
-                type="text"
-                id="whatsapp"
-                value={form?.whatsapp}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <FormLabel htmlFor="instagram">Link Instagram:</FormLabel>
-            <div className="flex items-center">
-              <FaInstagram className="text-xl" />
-
-              <Input
-                className="w-full ms-2"
-                onChange={handleChange}
-                type="text"
-                id="instagram"
-                value={form?.instagram}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <FormLabel htmlFor="facebook">Link Facebook:</FormLabel>
-            <div className="flex items-center">
-              <FaFacebookSquare className="text-xl" />
-
-              <Input
-                className="w-full ms-2"
-                onChange={handleChange}
-                type="text"
-                id="facebook"
-                value={form?.facebook}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <FormLabel htmlFor="linkedin">Link Linkedin:</FormLabel>
-            <div className="flex items-center">
-              <FaLinkedin className="text-xl" />
-
-              <Input
-                className="w-full ms-2"
-                onChange={handleChange}
-                type="text"
-                id="linkedin"
-                value={form?.linkedin}
-                required
-              />
-            </div>
-          </div>
-        </div>
-
-        <FloatingButtonList
-          buttons={[
-            {
-              loading: loading,
-              onClick: handleSave,
-              icon: <FaSave />,
-            },
-          ]}
-        />
-      </div>
-    </div>
-  );
-
   return (
-    <PageStructure
-      loading={loading && !userData}
-      content={content}
-      contentHeader={contentHeader}
-      header={header}
-    />
+    <>
+      <PageStructure
+        loading={loading && !userData}
+        content={userData && content}
+        header={header}
+      />
+      <UserFormModal
+        editUser={userData}
+        open={userFormModal}
+        onClose={() => {
+          dispatch(fetchUserData());
+          setUserFormModal(false);
+        }}
+      />
+    </>
   );
 }
