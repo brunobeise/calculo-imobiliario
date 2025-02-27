e6860f3b58f83a20f6483361b75be35e52ddaf68
brunobeise <bruno_beise@hotmail.com>
2025-02-27 11:51:48 -0300

    ssr

---
+++ api/ssr.ts
@@ -0,0 +1,16 @@
+import { renderPage } from "vike";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const pageContext = await renderPage({ urlOriginal: req.url! });
+
+  if (pageContext.httpResponse) {
+    const { body, statusCode, headers } = pageContext.httpResponse;
+    headers.forEach(([name, value]) => res.setHeader(name, value));
+    res.statusCode = statusCode;
+    res.end(body);
+  } else {
+    res.statusCode = 404;
+    res.end("Page not found");
+  }
+}
