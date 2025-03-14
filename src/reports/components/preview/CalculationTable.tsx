3ffd22727cbbecbb84178d0164d2a178f4d4d227
brunobeise <bruno_beise@hotmail.com>
2025-03-14 10:02:40 -0300

    calcualtion table fix

--- src/reports/components/preview/CalculationTable.tsx
+++ src/reports/components/preview/CalculationTable.tsx
@@ -2,6 +2,7 @@ import { toBRL } from "@/lib/formatter";
 import SectionTitle from "./SectionTitle";
 import { GrLineChart } from "react-icons/gr";
 import { PropertyData } from "@/propertyData/PropertyDataContext";
+import dayjs from "dayjs";
 
 interface CalculationTableProps {
   propertyData: PropertyData;
@@ -14,7 +15,6 @@ interface CalculationTableProps {
   };
   color: string;
   secondary: string;
-  displayRentalAmount?: boolean;
 }
 
 const CalculationTable = ({
@@ -22,14 +22,17 @@ const CalculationTable = ({
   caseData,
   color,
   secondary,
-  displayRentalAmount = true,
 }: CalculationTableProps) => {
+  const hasNonZeroRent = caseData.detailedTable.some(
+    (item) => item.rentValue !== 0
+  );
+
   return (
     <>
       <SectionTitle
         color={color}
         secondary={secondary}
-        title={"Valorização do Imóvel e Projeção do Aluguel"}
+        title={"Valorização do Imóvel"}
         icon={<GrLineChart />}
       />
       <table
@@ -41,10 +44,14 @@ const CalculationTable = ({
             <th
               style={{ borderColor: secondary }}
               className="px-4 py-3 border-r border-b text-left"
-            ></th>
+            >
+              Ano
+            </th>
             <th
               style={{ borderColor: secondary }}
-              className="px-4 py-3 border-r border-b text-left"
+              className={`px-4 py-3 ${
+                hasNonZeroRent ? "border-r" : ""
+              } border-b text-left`}
             >
               <div className="flex flex-col">
                 <strong>Valorização Imóvel</strong>
@@ -53,31 +60,31 @@ const CalculationTable = ({
                 </span>
               </div>
             </th>
-            <th
-              style={{ borderColor: secondary }}
-              className={`px-4 py-3 text-left ${
-                !displayRentalAmount ? "" : "border-r"
-              }`}
-            >
-              <div className="flex flex-col">
-                <strong>Projeção Aluguel</strong>
-                <span className="text-sm font-normal">
-                  {propertyData.rentAppreciationRate}%
-                </span>
-              </div>
-            </th>
-            {displayRentalAmount && (
-              <th
-                style={{ borderColor: secondary }}
-                className={`px-4 py-3 border-b text-left ${
-                  displayRentalAmount ? "" : "border-r"
-                }`}
-              >
-                <div className="flex flex-col">
-                  <strong>Diferença</strong>
-                  <span className="text-sm font-normal">aluguel - parcela</span>
-                </div>
-              </th>
+            {hasNonZeroRent && (
+              <>
+                <th
+                  style={{ borderColor: secondary }}
+                  className="px-4 py-3 border-r border-b text-left"
+                >
+                  <div className="flex flex-col">
+                    <strong>Projeção Aluguel</strong>
+                    <span className="text-sm font-normal">
+                      {propertyData.rentAppreciationRate}%
+                    </span>
+                  </div>
+                </th>
+                <th
+                  style={{ borderColor: secondary }}
+                  className="px-4 py-3 border-b text-left"
+                >
+                  <div className="flex flex-col">
+                    <strong>Diferença</strong>
+                    <span className="text-sm font-normal">
+                      aluguel - parcela
+                    </span>
+                  </div>
+                </th>
+              </>
             )}
           </tr>
         </thead>
@@ -90,32 +97,37 @@ const CalculationTable = ({
                     style={{ color, borderColor: secondary }}
                     className="px-4 py-3 border-r border-t font-bold"
                   >
-                    Ano {(i - 1) / 12 + 1}
+                    {dayjs(propertyData.initialDate, "MM/YYYY")
+                      .add((i - 1) / 12, "year")
+                      .locale("pt-br")
+                      .format("YYYY")}
                   </td>
                   <td
                     style={{ color, borderColor: secondary }}
-                    className="px-4 py-3 border-r border-t"
+                    className={`px-4 py-3  border-t ${
+                      hasNonZeroRent ? "border-r" : ""
+                    }`}
                   >
                     {toBRL(
                       item.propertyValue *
                         (1 + propertyData.propertyAppreciationRate / 100)
                     )}
                   </td>
-                  <td
-                    style={{ color, borderColor: secondary }}
-                    className={`px-4 py-3 border-t ${
-                      displayRentalAmount ? "border-r" : ""
-                    }`}
-                  >
-                    {toBRL(item.rentValue)}
-                  </td>
-                  {displayRentalAmount && (
-                    <td
-                      style={{ color, borderColor: secondary }}
-                      className="px-4 py-3 border-t"
-                    >
-                      {toBRL(item.rentalAmount)}
-                    </td>
+                  {hasNonZeroRent && (
+                    <>
+                      <td
+                        style={{ color, borderColor: secondary }}
+                        className="px-4 py-3 border-r border-t"
+                      >
+                        {toBRL(item.rentValue)}
+                      </td>
+                      <td
+                        style={{ color, borderColor: secondary }}
+                        className="px-4 py-3 border-t"
+                      >
+                        {toBRL(item.rentalAmount)}
+                      </td>
+                    </>
                   )}
                 </tr>
               );
