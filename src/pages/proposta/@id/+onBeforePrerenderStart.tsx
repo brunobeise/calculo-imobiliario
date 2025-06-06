1f59a8dd561fa9accf6b0f8195cfb9ded04718d1
brunobeise <bruno_beise@hotmail.com>
2025-06-06 17:26:08 -0300

    att 06.06

--- src/pages/proposta/@id/+onBeforePrerenderStart.tsx
+++
@@ -1,8 +0,0 @@
-// import { caseService } from "@/service/caseService";
-
-// export async function onBeforePrerenderStart() {
-//   const proposals = await caseService.getAllProposals();
-//   return proposals?.map((caseItem) => ({
-//     url: `/proposta/${caseItem}`,
-//   }));
-// }
